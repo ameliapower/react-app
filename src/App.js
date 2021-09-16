e65c5cb4ec07e7a118dce27b7515f22f00e0b6ea
POWERAMT <amelia.power@utc.com>
2021-09-15 22:34:17 -0400

    adds scss file, sass package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import './sample.css';
+import './sample.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Red header</h2>
+		<button className="button">click</button>
 		<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
