35050ef5a3238d18a4b911d561288841e12818ef
POWERAMT <amelia.power@utc.com>
2021-09-15 22:24:31 -0400

    reinstalled packages after babel eject error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './sample.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h2>Red header</h2>
+		<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
